963fa90f34c8aec79778d3e9fbc4ce5ee429c5af
Astghik Abovyan <astghikabovyan28@gmail.com>
2025-05-26 09:46:14 +0400

    new

--- app/src/main/java/app/psy/innergrowth/DetailActivity.java
+++ app/src/main/java/app/psy/innergrowth/DetailActivity.java
@@ -1,30 +1,23 @@
 package app.psy.innergrowth;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.github.clans.fab.FloatingActionButton;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView detailDesc, detailTitle, detailLang;
-    ImageView detailImage;
     FloatingActionButton deleteButton, editButton;
     String key = "";
-    String imageUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +26,9 @@ public class DetailActivity extends AppCompatActivity {
 
         detailDesc = findViewById(R.id.detailDesc);
         detailTitle = findViewById(R.id.detailTitle);
+        detailLang = findViewById(R.id.detailLang);
         deleteButton = findViewById(R.id.deleteButton);
         editButton = findViewById(R.id.editButton);
-        detailLang = findViewById(R.id.detailLang);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null){
@@ -43,20 +36,16 @@ public class DetailActivity extends AppCompatActivity {
             detailTitle.setText(bundle.getString("Title"));
             detailLang.setText(bundle.getString("Language"));
             key = bundle.getString("Key");
-            imageUrl = bundle.getString("Image");
-            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
         }
+
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Android Tutorials");
-                FirebaseStorage storage = FirebaseStorage.getInstance();
 
-                StorageReference storageReference = storage.getReferenceFromUrl(imageUrl);
-                storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                reference.child(key).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
-                        reference.child(key).removeValue();
                         Toast.makeText(DetailActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
@@ -64,6 +53,7 @@ public class DetailActivity extends AppCompatActivity {
                 });
             }
         });
+
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,7 +61,6 @@ public class DetailActivity extends AppCompatActivity {
                         .putExtra("Title", detailTitle.getText().toString())
                         .putExtra("Description", detailDesc.getText().toString())
                         .putExtra("Language", detailLang.getText().toString())
-                        .putExtra("Image", imageUrl)
                         .putExtra("Key", key);
                 startActivity(intent);
             }
